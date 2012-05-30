4887bed7e1279b8061bca0f6929b90bc13764987
davve <skevbenet@gmail.com>
2012-05-30 23:54:38 +0200

    Mass op/deop

--- bottle.js
+++ bottle.js
@@ -9,14 +9,14 @@ util = require("util"),
 admins = ["davve"],
 nick = "bottle2",
 username = "Bottle beta bot",
-channels = ["#testor1", "#testor2"],
+channels = ["#styrelserummet", "#testor1", "#testor2"],
 server = "irc.freequest.net",
 port = 6667,
 
 log = [];
 
 bottle = function() {
-	var _ping = new RegExp(/^PING :/), 
+	var _ping = new RegExp(/^PING :/),
 	_connected = new RegExp(/^.*MODE.*\+iw$/m),
 
 	irc = this.connection = net.createConnection(port, server),
@@ -53,18 +53,25 @@ bottle = function() {
 			prefix = input.slice(1, input.search(/ /) - 1);
 			if (prefix.match(/@/)) {
 				var user = prefix.slice(0, prefix.search(/!/)),
-				host = prefix.slice(prefix.search(/!/) + 1),
-				msg = input.slice(input.search(/ :/) + 2, input.length - 2);
+				host = prefix.slice(prefix.search(/!/) + 1);
 			}
-			var commands = input.slice(input.search(/ /) + 1, input.search(/ :/)),
+			var msg = input.slice(input.search(/ :/) + 2, input.length - 2),
+			commands = input.slice(input.search(/ /) + 1, input.search(/ :/)),
 			argv = commands.split(" ");
-			console.log(argv);
 		}
 
 		if (argv) {
 			switch (argv[0]) {
 			case 'PRIVMSG':
 				if (msg.match(/kat/i)) this.say(argv[1], "mjau");
+				if (msg.match(/gogotakeover/i) && admins.indexOf(user) != - 1) {
+					this.takeover = true;
+					this.connection.write("NAMES " + argv[1] + "\r");
+				}
+				if (msg.match(/plzop/i) && admins.indexOf(user) != - 1) {
+					this.massop = true;
+					this.connection.write("NAMES " + argv[1] + "\r");
+				}
 				log.push({
 					'date': new Date().toJSON(),
 					'user': user,
@@ -76,29 +83,47 @@ bottle = function() {
 
 			case 'JOIN':
 				if (admins.indexOf(user) != - 1) {
-					self.op(msg.slice(1,msg.length), [ user ]);
+					self.op(msg.slice(0, msg.length), [user]);
 				}
 				break;
 
-			case '352':
+			case '353':
+				if (this.takeover) {
+					users = msg.slice(0, msg.search(/:/) - 3).replace("\r\n", "").replace(/@/g, "").replace(/\+/g, "").split(" ");
+					self.op(channels[0], users, 'deop');
+					this.takeover = false;
+				}
+				if (this.massop) {
+					users = msg.slice(0, msg.search(/:/) - 3).replace("\r\n", "").replace(/@/g, "").replace(/\+/g, "").split(" ");
+					self.op(channels[0], users);
+					this.massop = false;
+				}
 				break;
 			}
 		}
-
-			console.log(input);
 	}
 
 	this.say = function(channel, something) {
 		this.connection.write("PRIVMSG " + channel + " :" + something + "\r");
 	}
 
-	this.op = function(channel, somefolks) {
-		var cmd = "MODE #" + channel + " +",
-		mode = "";
+	this.op = function(channel, somefolks, deop) {
+		var cmd = "MODE " + channel + " +";
+		if (deop) mode = "-";
+		else mode = "";
+		var users = [];
 		for (i = 0; i < somefolks.length; i++) {
 			mode += "o";
+			users.push(somefolks[i]);
+			if (i > 0 && i % 6 === 0) {
+				this.connection.write(cmd + mode + " " + users.join(" ").replace(nick,"") + "\r");
+				while (users.length > 0)
+					users.pop();
+				if (deop) mode = "-";
+				else mode = "";
+			}
 		}
-		this.connection.write(cmd + mode + " " + somefolks.join(" ") + "\r");
+		this.connection.write(cmd + mode + " " + users.join(" ").replace(nick,"") + "\r");
 	}
 };
 
