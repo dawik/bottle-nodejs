967f6587876e4e736f424b6e059de0f942a7706c
davve <skevbenet@gmail.com>
2012-06-01 16:43:45 +0200

    all tabs

--- coffeeport.cs
+++ coffeeport.cs
@@ -5,7 +5,7 @@ util = require("util")
 admins = ["davve"]
 nick = "bottle"
 username = "Bottle beta bot"
-channels = ["#styrelserummet", "#testor1", "#testor2"]
+channels = ["#testor1", "#testor2"]
 server = "irc.freequest.net"
 port = 6667
 
@@ -20,98 +20,91 @@ bottle = ->
 	irc = @.connection = net.createConnection port, server
 
 	@.connection.on "error", (error) ->
-        console.log error
-        return
+		console.log error
 
 	@.connection.on "connect", ->
-        @.write "NICK " + nick + "\r"
-        @.write "USER bottle 8 * :" + username + "\r"
-        return
+		@.write "NICK " + nick + "\r"
+		@.write "USER bottle 8 * :" + username + "\r"
 
 	@.connection.on "data", (data) ->
-        text = data.toString()
-        if _ping.test text
-            @.write text.replace(/:/g, "") + "\r"
+		text = data.toString()
+		if _ping.test text
+			@.write text.replace(/:/g, "") + "\r"
 
-        if _connected.test text
-            for channel in channels
-                do (channel) ->
-                    self.connection.write "JOIN " + channel + "\r"
-                    self.say channel, "Howdee ho\r"
-                    return
-        else
-            self.parse text
-        return
+		if _connected.test text
+			for channel in channels
+				do (channel) ->
+					self.connection.write "JOIN " + channel + "\r"
+					self.say channel, "Howdee ho\r"
+		else
+			self.parse text
 
-    @.parse = (input) ->
-        if input.charAt 0 == ":"
-            prefix = input.slice 1, (input.search /\ /) - 1
+	@.parse = (input) ->
+		if input.charAt 0 == ":"
+			prefix = input.slice 1, (input.search /\ /) - 1
 
-            if prefix.match /@/
-                user = prefix.slice 0, prefix.search /!/
-                host = prefix.slice (prefix.search /!/) + 1
+			if prefix.match /@/
+				user = prefix.slice 0, prefix.search /!/
+				host = prefix.slice (prefix.search /!/) + 1
 
-            msg = input.slice (input.search /\ :/) + 2, input.length - 2
-            commands = input.slice (input.search /\ /) + 1, input.search /\ :/
-            argv = commands.split " "
+			msg = input.slice (input.search /\ :/) + 2, input.length - 2
+			commands = input.slice (input.search /\ /) + 1, input.search /\ :/
+			argv = commands.split " "
 
-        if argv
-            switch argv[0]
-                when "PRIVMSG" 
-                    if msg.match /kat/i 
-                        self.say argv[1], "mjau"
-                    if msg.match(/gogotakeover/i) and admins.indexOf(user) != -1
-                        @.massdeop = true
-                        @.connection.write "NAMES " + argv[1] + "\r"
-                    if msg.match(/plzop/i) and admins.indexOf(user) != -1
-                        @.massop = true
-                        @.connection.write "NAMES " + argv[1] + "\r"
+		if argv
+			switch argv[0]
+				when "PRIVMSG" 
+					if msg.match /kat/i 
+						self.say argv[1], "mjau"
+					if msg.match(/gogotakeover/i) and admins.indexOf(user) != -1
+						@.massdeop = true
+						@.connection.write "NAMES " + argv[1] + "\r"
+					if msg.match(/plzop/i) and admins.indexOf(user) != -1
+						@.massop = true
+						@.connection.write "NAMES " + argv[1] + "\r"
 
-                when "JOIN"
-                    if admins.indexOf(user) != -1
-                        self.op (msg.slice 0, msg.length), [ user ]
+				when "JOIN"
+					if admins.indexOf(user) != -1
+						self.op (msg.slice 0, msg.length), [ user ]
 
-                when "353"
-                    if this.massdeop or this.massop
-                        users = msg.slice 0, (msg.search /:/) - 3
-                        users = users.replace "\r\n", ""
-                        users = users.replace /@/g, ""
-                        users = users.replace /\+/g, ""
-                        users = users.split " "
-                        if @.massdeop
-                            self.op argv[3], users, "deop"
-                            @.massdeop = false
+				when "353"
+					if this.massdeop or this.massop
+						users = msg.slice 0, (msg.search /:/) - 3
+						users = users.replace "\r\n", ""
+						users = users.replace /@/g, ""
+						users = users.replace /\+/g, ""
+						users = users.split " "
+						if @.massdeop
+							self.op argv[3], users, "deop"
+							@.massdeop = false
 
-                        else
-                            self.op argv[3], users
-                            @.massop = false
+						else
+							self.op argv[3], users
+							@.massop = false
 
-        @.say = (channel, something) ->
-            @.connection.write "PRIVMSG " + channel + " :" + something + "\r"
+		@.say = (channel, something) ->
+			@.connection.write "PRIVMSG " + channel + " :" + something + "\r"
 
-        @.op = (channel, somefolks, deop) ->
-            cmd = "MODE " + channel + " "
-            users = [ ]
+		@.op = (channel, somefolks, deop) ->
+			cmd = "MODE " + channel + " "
+			users = [ ]
 
-            if deop
-                mode = "-"
-            else
-                mode = "+"
+			if deop
+				mode = "-"
+			else
+				mode = "+"
 
-            `for (i = 0; i < somefolks.length; i++) {
-	    		mode += "o";
-    			users.push(somefolks[i]);
-    			if (i > 0 && i % 6 === 0 || i === somefolks.length - 1) {
-    				this.connection.write(cmd + mode + " " + users.join(" ").replace(nick, "") + "\r");
-    				while (users.length > 0)
-    				    users.pop();
-    				if (deop) mode = "-";
-    				else mode = "+";
-    			}
-            }`
-            return;
-        return
-        
-    return
+			`for (i = 0; i < somefolks.length; i++) {
+				mode += "o";
+				users.push(somefolks[i]);
+				if (i > 0 && i % 6 === 0 || i === somefolks.length - 1) {
+					this.connection.write(cmd + mode + " " + users.join(" ").replace(nick, "") + "\r");
+					while (users.length > 0)
+						users.pop();
+					if (deop) mode = "-";
+					else mode = "+";
+				}
+			}`
+			return
 
 bot = new bottle
