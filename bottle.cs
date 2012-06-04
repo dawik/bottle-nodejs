4b2d5f28afd8a004c4ce5decc7fb1baeb1cb70fc
davve <skevbenet@gmail.com>
2012-06-04 03:18:56 +0200

    Modular support

--- bottle.cs
+++ bottle.cs
@@ -1,4 +1,5 @@
 net = require("net")
+fs = require("fs")
 
 admins = ["davve"]
 nick = "bottle_beta"
@@ -9,12 +10,25 @@ port = 6667
 
 log = []
 
+# Simple module that takes arguments in a vector and returns a formatted string
+# Black box style
+module_example = (argv, input) ->
+	jokes = fs.readFileSync("chuckfacts.txt", "utf8").split("\n")
+
+	if argv and argv[1] > 0 and argv[1] <= jokes.length 
+			number = argv[1]
+	else
+		number = Math.round(Math.random() * jokes.length)
+	"Chuck norris fact #" + number + ": " + jokes[number]
+
 bottle = ->
 	self = @
 
 	_me = new RegExp "^" + nick + ": (.*)"
 	_ping = new RegExp  /^PING\ :/
 	_connected = new RegExp /^.*MODE.*\+iw$/m
+
+	@.modules = { "chuck" : module_example, "rand" : Math.random }
 
 	irc = @.connection = net.createConnection port, server
 
@@ -55,6 +69,7 @@ bottle = ->
 					if _me.test trailing
 						cmd = _me.exec trailing
 						cmd_argv = cmd[1].split " "
+
 						switch cmd_argv[0]
 							when "hey"
 								self.say irc_argv[1], "sup"
@@ -64,7 +79,9 @@ bottle = ->
 									@.massflag = cmd_argv[1]
 									@.connection.write "NAMES " + irc_argv[1] + "\r"
 							else
-								self.say irc_argv[1], "you want something?\r"
+								for hook,fn of self.modules
+									if cmd_argv[0] == hook
+										self.say irc_argv[1], fn(cmd_argv, trailing)
 
 				when "JOIN"
 					if admins.indexOf(user) != -1
